e5d3855a376a6f735943ab2ff32863b972660e5b
Saish Patil <98648631+Saish40@users.noreply.github.com>
2025-03-08 16:04:05 +0530

    Create lambda.py

---
+++ lambda.py
@@ -0,0 +1,30 @@
+import json
+import boto3
+
+client = boto3.client("bedrock-runtime")
+
+def lambda_handler(event, context):
+
+    print(event)
+    user_prompt = event['prompt']
+
+    response = client.invoke_model(
+        body = json.dumps(
+            {
+                "prompt" : user_prompt,
+                "temperature" : 0.9,
+                "max_tokens" : 4000,
+            }
+        ),
+        contentType = 'application/json'
+        accept = 'application/json'
+        modelId = 'cohere.command-text-v14',
+    )
+    print(response['body'])
+    response_bytes = response['body'].read()
+    response_string = json.loads(response_bytes)
+
+    return {
+        'statusCode': 200,
+        'body': response_string
+    }
